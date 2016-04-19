85fc64a4b99440625c14a9cf6994d4f7193f7e1e
Ignacio Jorge <nacho108>
2016-04-19 18:25:28 +0300

    Changed output redirect approach from async to sync events

--- DeploymentManager/DeploymentFlow/Commands/ShellCommand.cs
+++ DeploymentManager/DeploymentFlow/Commands/ShellCommand.cs
@@ -1,14 +1,27 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using DeploymentFlow.Annotations;
 
-namespace DeploymentFlow
+namespace DeploymentFlow.Commands
 {
-    public class ShellCommand : ICommand
+    public class ShellCommand : ICommand, INotifyPropertyChanged
     {
+        private string _output;
+        public string Output
+        {
+            get { return _output; }
+            set
+            {
+                if (value == _output) return;
+                _output = value;
+                OnPropertyChanged();
+            }
+        }
+
         public async Task<CommandResult> Execute()
         {
-            string standardOutput ="";
-            string standardError ;
             int exitCode=9;
             await Task.Run(() =>
             {
@@ -19,24 +32,31 @@ namespace DeploymentFlow
                     process.StartInfo.RedirectStandardError = true;
                     process.StartInfo.RedirectStandardOutput = true;
                     process.StartInfo.FileName = "..\\..\\batchs\\test.bat";
-
+                    process.OutputDataReceived += Process_OutputDataReceived;
                     process.Start();
-
-                    //Thread.Sleep(100);
+                    process.BeginOutputReadLine();
 
                     using (Task processWaiter = Task.Run(() => process.WaitForExit()))
-                    using (Task<string> outputReader = Task.Run(() => process.StandardOutput.ReadToEnd()))
-                    using (Task<string> errorReader = Task.Factory.StartNew(() => process.StandardError.ReadToEnd()))
                     {
-                        Task.WaitAll(processWaiter, outputReader, errorReader);
-
-                        standardOutput = outputReader.Result;
-                        standardError = errorReader.Result;
+                        Task.WaitAll(processWaiter);
                         exitCode = process.ExitCode;
                     }
                 }
             });
-            return new CommandResult(exitCode, standardOutput);
+            return new CommandResult(exitCode, _output);
+        }
+
+        private void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            _output += e.Data+"\n";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
