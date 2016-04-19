011b2635f2cf1717065cf8379414b3b6816f23dc
Nacho <nacho.jorge@gmail.com>
2016-04-19 09:16:57 +0300

    *

---
+++ DeploymentManager/DeploymentManager/Batchs/test.bat
@@ -0,0 +1,14 @@
+cd C:\Projects\Trueview
+rem @if %errorlevel% neq 0 exit /b %errorlevel%
+git checkout master
+rem @if %errorlevel% neq 0 exit /b %errorlevel%
+rem @if %errorlevel% neq 0 exit /b %errorlevel%
+rem @if %errorlevel% neq 0 exit /b %errorlevel%
+git checkout develop
+rem @if %errorlevel% neq 0 exit /b %errorlevel%
+
+
+
+
+
+
