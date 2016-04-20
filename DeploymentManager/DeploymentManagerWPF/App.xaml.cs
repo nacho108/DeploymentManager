e62a01896c5ce39a6a2a61c322596df0253a01e7
Nacho <nacho.jorge@gmail.com>
2016-04-20 09:09:01 +0300

    *

--- DeploymentManager/DeploymentManagerWPF/App.xaml.cs
+++ DeploymentManager/DeploymentManagerWPF/App.xaml.cs
@@ -19,11 +19,13 @@ namespace DeploymentManager
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             var stepList = new List<FlowStep>();
-            Directory.SetCurrentDirectory(@"C:\Projects\Trueview");
-            var step2 = new FlowStep(new ShellCommand("git.exe", "checkout develop"), "Checkout Develop branch", 0);
+
             //var step3 = new FlowStep(new AskCommand(new WpfQuestion("https://www.google.com.ua/?gfe_rd=cr&ei=9GMUV5-jC86BtAHIm4_4Aw&gws_rd=ssl ")), "Third step", 2);
 
-            stepList.Add(step2);
+            stepList.Add(new FlowStep(new NullCommand("first"), "something", 0));
+            stepList.Add(new FlowStep(new ShellCommand("cmd.exe", "/C git -C C:\\Projects\\testing checkout develop"), "Checkout local Develop branch", 0));
+            stepList.Add(new FlowStep(new ShellCommand("cmd.exe", "/C git -C C:\\Projects\\testing pull"), "Pull remote", 0));
+
             var workFlowProvider = new WorkFlowProvider(stepList);
             MainViewModel mainviewModel = new MainViewModel(workFlowProvider);
             MainWindow mw = new MainWindow() { DataContext = mainviewModel };
