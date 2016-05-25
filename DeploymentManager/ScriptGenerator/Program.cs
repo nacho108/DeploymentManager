f7bd721b2f871693308926c5c8cc2376af5fc4db
Nacho <nacho.jorge@gmail.com>
2016-05-25 22:04:05 +0300

    *

--- DeploymentManager/ScriptGenerator/Program.cs
+++ DeploymentManager/ScriptGenerator/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommandLine;
+using ScriptCreator;
 
 namespace ScriptGenerator
 {
@@ -14,19 +15,21 @@ namespace ScriptGenerator
             Console.WriteLine("*********************");
             var generatorArguments = new GeneratorArguments();
 
-            //foreach (var s in args)
-            //{
-            //    if (s == "?" || s == "--?" || s == "-?")
-            //    {
-            //        //ShowHelp();      
-            //        return;
-            //    }
-            //}
-
             if (!Parser.Default.ParseArguments(args, generatorArguments))
             {
                 return;
             }
+
+            int mayorVersion, minorVersion, build;
+
+            if (!int.TryParse(generatorArguments.MayorVersion, out mayorVersion)) throw new ArgumentException("The system was not able to understand the mayor version argument !!");
+            if (!int.TryParse(generatorArguments.MinorVersion, out minorVersion)) throw new ArgumentException("The system was not able to understand the minor version argument !!");
+            if (!int.TryParse(generatorArguments.Build, out build)) throw new ArgumentException("The system was not able to understand the build version argument !!");
+
+            var currentVersionWriter = new CurrentVersionWriter(generatorArguments.DatabaProjectFolder);
+            var currentVersionProvider = new CurrentVersionProvider(generatorArguments.DatabaProjectFolder);
+            var storeProceduresCreatorCommand = new StoreProceduresCreatorCommand(generatorArguments.DatabaProjectFolder, 
+                new ScriptProvider(), generatorArguments.RequiredVersion, mayorVersion, minorVersion,build, 0, currentVersionWriter);
         }
     }
 }
